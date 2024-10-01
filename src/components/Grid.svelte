c1fb069ac52a49572ec4d26f5715b86067731685
Chad Gauthier <chad.gauthier32@gmail.com>
2024-10-01 19:58:54 -0400

    first commit

---
+++ src/components/Grid.svelte
@@ -0,0 +1,3 @@
+<div class="flex justify-center items-center flex-wrap w-full h-full">
+    <slot/>
+</div>
---
+++ src/components/GridItem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let title: string;
+    export let link: string;
+    export let image: string;
+</script>
+
+<div class="card card-compact bg-base-100 w-96 shadow-xl m-3">
+    <figure>
+        <img
+                src="{image}"
+                alt="{title}"
+                class="aspect-square"
+        />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">{title}</h2>
+        <div class="card-actions justify-end">
+            <a href="{link}" target="_blank" class="btn btn-primary">Buy Now</a>
+        </div>
+    </div>
+</div>
