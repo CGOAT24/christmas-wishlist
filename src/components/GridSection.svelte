350a0ae323b58e42078dbacca67e973458bba9b9
Chad Gauthier <chad.gauthier32@gmail.com>
2024-11-13 17:17:46 -0500

    added categories

---
+++ src/components/GridSection.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import GridItem from "./GridItem.svelte";
+
+    export let category: string = "General";
+    export let items: {title: string, link: string, image: string, category: string | undefined}[];
+</script>
+<div class="divide-y w-full">
+    <h2 class="text-left m-3 w-full">{category}</h2>
+    <div class="flex justify-between items-center flex-wrap w-full h-full">
+        {#each items as item}
+            <GridItem title={item.title} link={item.link} image={item.image} />
+        {/each}
+    </div>
+
+</div>
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Liste de souhait de Chad';
-export const SITE_DESCRIPTION = 'Liste de souhait de Chad!';
+export const SITE_TITLE = 'Chad\'s wishlist';
