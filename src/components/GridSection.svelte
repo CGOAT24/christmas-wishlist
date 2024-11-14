3745f196eba9d4da3a1fda000becb8cea4c9714c
Chad Gauthier <chad.gauthier32@gmail.com>
2024-11-13 21:39:18 -0500

    update data

--- src/components/GridSection.svelte
+++ src/components/GridSection.svelte
@@ -6,7 +6,7 @@
 </script>
 <div class="divide-y w-full">
     <h2 class="text-left m-3 w-full">{category}</h2>
-    <div class="flex justify-between items-center flex-wrap w-full h-full">
+    <div class="flex justify-evenly items-center flex-wrap w-full h-full">
         {#each items as item}
             <GridItem title={item.title} link={item.link} image={item.image} />
         {/each}
